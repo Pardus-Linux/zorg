198d29f41aed7d88b9dd9a673ff8bc22efeb2822
Fatih Aşıcı <fatih@pardus.org.tr>
2007-09-05 07:59:45 +0000

    Log parsing code for ATI proprietary driver

--- zorg/probe.py
+++ zorg/probe.py
@@ -336,7 +336,66 @@ def queryNvidiaOutputs(device):
                     parsingModesFor = ""
 
 def queryFglrxOutputs(device):
-    pass
+    lines = xserverProbe(device)
+    if not lines:
+        return
+
+    device.tvStandards = [
+            "NTSC-M",       "NTSC-JPN", "NTSC-N",      "PAL-B",
+            "PAL-COMB-N",   "PAL-D",    "PAL-G",       "PAL-H",
+            "PAL-I",        "PAL-K",    "PAL-K1",      "PAL-L",
+            "PAL-M",        "PAL-N",    "PAL-SECAM-D", "PAL-SECAM-K",
+            "PAL-SECAM-K1", "PAL-SECAM-L"
+        ]
+
+    outInfo = re.compile(r".*: Connected Display\d+: (.+) \[(.+)\].*")
+    modeCount = re.compile(r".*: Total of \d+ modes found for .* display.*")
+    modeLine = re.compile(r".*: Modeline \"(.*)\" *.*")
+
+    primary = ""
+    secondary = ""
+    outs = []
+    parsingModesFor = ""
+
+    for line in lines:
+        if "Connected Display" in line:
+            matched = outInfo.match(line)
+            if matched:
+                info = matched.groups()
+                outs.append(info)
+                device.outputs[info[1]] = []
+
+        if "Primary Controller - " in line:
+            for out in outs:
+                if out[0] in line:
+                    primary = out[1]
+
+        if "Secondary Controller - " in line:
+            for out in outs:
+                if out[0] in line:
+                    secondary = out[1]
+
+        elif "modes found for primary display" in line:
+            matched = modeCount.match(line)
+            if matched:
+                parsingModesFor = primary
+
+        elif "modes found for secondary display" in line:
+            matched = modeCount.match(line)
+            if matched:
+                parsingModesFor = secondary
+
+        elif parsingModesFor:
+            if "Display dimensions" in line \
+                    or "DPI" in line:
+                parsingModesFor = ""
+                continue
+
+            matched = modeLine.match(line)
+            if matched:
+                mode = matched.groups()[0]
+                if not mode in device.outputs[parsingModesFor]:
+                    device.outputs[parsingModesFor].append(mode)
 
 def xserverProbe(card):
     p = XorgParser()
