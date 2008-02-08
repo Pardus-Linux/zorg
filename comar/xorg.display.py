de4127970611ec19f1dbe7bbf429bb65b759c352
Fatih Aşıcı <fatih@pardus.org.tr>
2008-02-08 11:51:26 +0000

    Add new methods

--- comar/xorg.display.py
+++ comar/xorg.display.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from zorg.config import *
 from zorg.probe import *
 from zorg.utils import *
@@ -27,6 +29,49 @@ def safeConfig():
 
     device.monitor_settings["default-hsync"] = "31.5-50"
     device.monitor_settings["default-vref"] = "50-70"
+
+    saveXorgConfig(device)
+    saveDeviceInfo(device)
+
+def activeDeviceID():
+    if not os.path.exists(xorgConf):
+        return
+
+    parser = XorgParser()
+    parser.parseFile(xorgConf)
+
+    devSec = parser.getSections("Device")[0]
+    busId = devSec.get("BusId")
+
+    return busId
+
+def changeDriver(busId, driver):
+    pass
+
+def setupScreens(busId, options, firstScreen, secondScreen):
+    device = getDeviceInfo(busId)
+
+    if not device:
+        return
+
+    dsetup = options.get("desktop-setup", "single")
+
+    if dsetup not in ("single", "mirror", "horizontal", "vertical"):
+        return
+
+    device.desktop_setup = dsetup
+
+    if options.has_key("depth"):
+        device.depth = options["depth"]
+
+    device.active_outputs = [firstScreen["output"]]
+    device.modes[firstScreen["output"]] = firstScreen["mode"]
+
+    if dsetup != "single":
+        device.active_outputs.append(secondScreen["output"])
+        device.modes[secondScreen["output"]] = secondScreen["mode"]
+
+    device.requestDriverOptions()
 
     saveXorgConfig(device)
     saveDeviceInfo(device)
